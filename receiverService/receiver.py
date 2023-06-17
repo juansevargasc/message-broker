cd4c2f22e2c707bcdfbdacbf45d62e2d2c0aed26
Juanse Vargas MBP16 <jsvargasc@unal.edu.co>
2023-06-16 21:07:33 -0500

    :sparkles: Reading messages from console!
    It contains the functionality for sending messages input from the console.

--- receiverService/receiver.py
+++ receiverService/receiver.py
@@ -1,25 +1,28 @@
 import pika
 
-
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="localhost"))
-
-channel = connection.channel()
-
-channel.exchange_declare(exchange="logs", exchange_type="fanout")
-
-result = channel.queue_declare(queue="", exclusive=True)
-
-queue_name = result.method.queue
-
-channel.queue_bind(exchange="logs", queue=queue_name)
-
-print(" [*] Waiting for logs. To exit press CTRL+C ")
+HOST = "localhost"
 
 
-def callback(ch, method, properties, body):
-    print(" [x] %r" % body)
+if __name__ == "__main__":
 
+    # 1. Establish connection with RabbitMQ server
+    connection = pika.BlockingConnection( pika.ConnectionParameters(host=HOST) )
 
-channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    # 2. Create a channel
+    channel = connection.channel()
+    channel.exchange_declare(exchange="logs", exchange_type="fanout")
+    result = channel.queue_declare(queue="", exclusive=True)
 
-channel.start_consuming()
+    # 3. Declare a queue
+    softArchQueue = result.method.queue
+    channel.queue_bind(exchange="logs", queue=queue_name)
+
+    print(" [*] Waiting for logs. To exit press CTRL+C ")
+
+    # 4. Define a callback function
+    def callback(ch, method, properties, body):
+        print(" [x] %r" % body)
+
+    #  5. Consume messages from the queue
+    channel.basic_consume(queue=softArchQueue, on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
